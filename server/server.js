920567db650f4aec0cabac4e870f6768adc2128f
gary1030 <gary6658@ntu.im>
2021-01-21 18:04:49 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const typeDefs = require('./graphql/typeDefs.js');
 const resolvers = require('./graphql/resolvers')
 
 const pubsub = new PubSub();
+const PORT = process.env.PORT || 5000
 
 const server = new ApolloServer({
     typeDefs,
@@ -38,7 +39,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useFindAndModify: false
 }).then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
 }).then((res) => {
     console.log(`Server running at ${res.url}`);  
 });
